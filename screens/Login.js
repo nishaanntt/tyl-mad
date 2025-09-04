9e35aba50f0eb92b6566e8cc9918224b0550bed4
nishaanntt <nparashar1998@gmail.com>
2025-09-04 17:28:46 +0530

    login UI

---
+++ screens/Login.js
@@ -0,0 +1,97 @@
+import {
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+const Login = () => {
+	return (
+		<SafeAreaView style={[styles.safeContainer]}>
+			<View style={styles.container}>
+				<Text style={styles.logo}>CMR Institute of Technology</Text>
+				<View style={styles.formContainer}>
+					<View style={styles.field}>
+						<Text style={styles.label}>Email</Text>
+						<TextInput
+							placeholder='abc@example.com'
+							keyboardType='email-address'
+							style={styles.input}
+						/>
+					</View>
+					<View style={styles.field}>
+						<Text style={styles.label}>Password</Text>
+						<TextInput
+							placeholder='password'
+							secureTextEntry
+							style={styles.input}
+						/>
+					</View>
+					<TouchableOpacity>
+						<View style={styles.button}>
+							<Text style={styles.buttonText}>Login</Text>
+						</View>
+					</TouchableOpacity>
+					<View style={styles.registerContainer}>
+						<Text>Don't have an account yet? </Text>
+						<TouchableOpacity>
+							<Text style={styles.linkText}>Register here</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
+};
+export default Login;
+
+const styles = StyleSheet.create({
+	safeContainer: {
+		flex: 1,
+	},
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+	},
+	logo: {
+		textAlign: 'center',
+		fontSize: 24,
+		fontWeight: '600',
+		marginBottom: 50,
+	},
+	formContainer: {
+		padding: 16,
+	},
+	field: {
+		marginBottom: 15,
+	},
+	label: {
+		marginBottom: 5,
+	},
+	input: {
+		backgroundColor: '#eee',
+		padding: 10,
+		borderRadius: 10,
+	},
+	button: {
+		backgroundColor: 'navy',
+		padding: 16,
+		borderRadius: 10,
+		marginTop: 50,
+	},
+	buttonText: {
+		color: '#eee',
+		textAlign: 'center',
+		fontWeight: 'bold',
+	},
+	registerContainer: {
+		flexDirection: 'row',
+		marginTop: 10,
+		justifyContent: 'center',
+	},
+	linkText: {
+		color: 'maroon',
+		fontWeight: 'bold',
+	},
+});
---
+++ screens/Register.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const Register = () => {
+	return (
+		<SafeAreaView>
+			<Text>Register</Text>
+		</SafeAreaView>
+	);
+};
+export default Register;
+const styles = StyleSheet.create({});
